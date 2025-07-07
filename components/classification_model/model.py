0a8d1d34f5afcb7ddbf75ba809b39f03cd5b3d4d
rafaelkadyrov <rafkadyrov@gmail.com>
2025-07-07 20:54:24 +0700

    added jupyter notebook with training experiments

---
+++ components/classification_model/model.py
@@ -0,0 +1,13 @@
+"""This module contains a class for Pytorch model."""
+from torch import nn
+
+
+class FFNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
